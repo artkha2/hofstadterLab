4071a7bda100494ba5a5e2fe656aa44d979ad9b5
Artem Khaiet <timkhaiet@gmail.com>
2025-01-29 18:36:46 -0600

    added optimized approach

--- hofstadterLab.cpp
+++ hofstadterLab.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -6,6 +7,15 @@ int G(int n){
     if(n==0)
         return 0;
     return n-G(G(n-1));
+}
+
+// Optimized (non-recursive) function
+int optimized_G(int n) {
+    vector<int> seq(n + 1, 0); // all-zero (empty) array of size n+1 so that the last element has index n
+    for (int i = 1; i <= n; i++) {
+        seq[i] = i - seq[seq[i - 1]];
+    }
+    return seq[n];
 }
 
 int main() {
